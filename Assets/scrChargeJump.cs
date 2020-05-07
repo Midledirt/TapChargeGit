b147ea5d1d156ccbefe603316e891aa5e436a89b
Jonathan <jonpres@icloud.com>
2020-05-07 15:49:02 +0200

    These are the files from my prototype project. I simply opened the folder and copied over everything from it into the folder of this project. It is the only way I`ve found to make a git repository of an already existing Unity project. Hope this works... And if it does not, well I have just barely began working on this prototype, so no matter.

---
+++ Assets/scrChargeJump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrChargeJump : MonoBehaviour
+{
+    private void Update()
+    {
+        //Lets check for screen touches
+        if (Input.touchCount > 0)
+        {
+            print("Touching the screen");
+            //Store the first touch whenever 1 or more fingers hit the screen
+            Touch theTouch = Input.GetTouch(0);
+        }
+    }
+}
