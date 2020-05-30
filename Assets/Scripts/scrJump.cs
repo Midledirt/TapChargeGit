43b98d3faac8486eb68754abdb08cec0e5111c30
Jonathan <jonpres@icloud.com>
2020-05-30 14:42:25 +0200

    Added some comments to the code, and added a range for the default height of the jump.

--- Assets/Scripts/scrJump.cs
+++ Assets/Scripts/scrJump.cs
@@ -7,8 +7,11 @@ public class scrJump : MonoBehaviour
     [Tooltip("Slot the player into this slot")]
     public Transform Player;
 
-    public float jumpheight;
-    private float defaultJumpheight = 5f;
+    private float jumpheight;
+    [Header("Set default Jump Height")]
+    [SerializeField]
+    [Range(0.1f, 1f)]
+    private float defaultJumpheight = 0.1f;
     public bool isJumping = false;
 
     public bool Grounded;
@@ -18,13 +21,16 @@ public class scrJump : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //Calculate jump: Jumpheight + tapCount.
         jumpheight = (defaultJumpheight + GetComponent<scrTapCharge>().tapCount) * Time.deltaTime;
         if (isJumping == true)
         {
+            //Execute the jump
             StartCoroutine(executeJump());
         }
     }
 
+    //Ground check and animation
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ground"))
@@ -44,6 +50,7 @@ public class scrJump : MonoBehaviour
 
     public void Jump()
     {
+        //Very simple jump function
         Player.Translate(0, jumpheight, 0);
     }
 
